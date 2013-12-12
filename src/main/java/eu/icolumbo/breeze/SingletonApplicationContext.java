e1b0de67cc34f3398782b4b6a61d4d1724c6d085
jethrobakker <jethrobakker@gmail.com>
2013-12-12 13:53:27 +0100

    load correct xml context based on topology name (not topology instance id)

--- src/main/java/eu/icolumbo/breeze/SingletonApplicationContext.java
+++ src/main/java/eu/icolumbo/breeze/SingletonApplicationContext.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertySource;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -41,7 +42,7 @@ public enum SingletonApplicationContext {
 		ApplicationContext entry = INSTANCE.registry.get(id);
 		if (entry == null) {
 			logger.debug("Need new application context");
-			entry = instantiate(properties, topologyContext);
+			entry = instantiate(properties);
 			logger.info("Application context instantiated for topology '{}'", id);
 			INSTANCE.registry.put(id, entry);
 		}
@@ -49,12 +50,13 @@ public enum SingletonApplicationContext {
 		return entry;
 	}
 
-	private static ApplicationContext instantiate(Map<String, Object> stormConf, TopologyContext topologyContext) {
-		String[] configLocations = {format("classpath:/%s-context.xml", topologyContext.getStormId())};
+	private static ApplicationContext instantiate(Map<String, Object> stormConf) {
+		String topologyName = (String) stormConf.get("topology.name");
+		String[] configLocations = {format("classpath:/%s-context.xml", topologyName)};
 		AbstractApplicationContext result = new ClassPathXmlApplicationContext(configLocations, false);
 		result.setId(configLocations[0]);
 
-		if (stormConf != null && ! stormConf.isEmpty()) {
+		if (!stormConf.isEmpty()) {
 			logger.debug("Applying Storm configuration: {}", stormConf);
 			PropertySource propertySource = new MapPropertySource("storm-configuration", stormConf);
 			result.getEnvironment().getPropertySources().addLast(propertySource);
