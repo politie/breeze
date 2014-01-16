df711115501306586eb2ab3b0feece687fc2df3e
Pascal S. de Kloe <pascal@quies.net>
2014-01-16 23:28:16 +0100

    Cleanup sorry state.

--- src/test/java/eu/icolumbo/breeze/build/TopologyCompilationTest.java
+++ src/test/java/eu/icolumbo/breeze/build/TopologyCompilationTest.java
@@ -2,16 +2,22 @@ package eu.icolumbo.breeze.build;
 
 import eu.icolumbo.breeze.SpringBolt;
 import eu.icolumbo.breeze.SpringSpout;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import static java.util.Arrays.asList;
 import static java.util.Arrays.sort;
 import static java.util.Collections.EMPTY_LIST;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 
@@ -39,61 +45,70 @@ public class TopologyCompilationTest {
 
 	@Test
 	public void follow() {
-		SpringSpout spout = new SpringSpout(Bean.class, "f()", "feed");
-		SpringBolt bolt = new SpringBolt(Bean.class, "f(feed)", "ignored");
-		test(spout, bolt);
+		subject.add(spout("s1", "f()", "feed"));
+		subject.add(bolt("b1", "f(feed)", "ignored"));
+		subject.run();
+		subject.verify();
+
+		assertPipeline("s1", "b1");
+		assertPassThrough("b1");
 	}
 
 	@Test
-	public void splitup() {
-		SpringSpout spout = new SpringSpout(Bean.class, "f()", "feed");
-		SpringBolt bolt1 = new SpringBolt(Bean.class, "f(feed)", "a");
-		SpringBolt bolt2 = new SpringBolt(Bean.class, "f(feed)", "b");
-		SpringBolt bolt3 = new SpringBolt(Bean.class, "f(b)", "y");
-		SpringBolt bolt4 = new SpringBolt(Bean.class, "f(b)", "z");
-		test(spout, bolt1, bolt2, bolt3, bolt4);
+	public void splitUp() {
+		subject.add(spout("s1", "f()", "feed"));
+		subject.add(bolt("b1", "f(feed)", "a"));
+		subject.add(bolt("b2", "f(feed)", "b"));
+		subject.add(bolt("b3", "f(b)", "y"));
+		subject.add(bolt("b4", "f(b)", "z"));
+
+		assertPipeline("s1", "b1", "b2", "b3", "b4");
+		assertPassThrough("b1", "feed");
+		assertPassThrough("b2");
+		assertPassThrough("b3", "b");
+		assertPassThrough("b4");
 	}
 
 
 	@Test
 	public void join() {
-		SpringSpout spout = new SpringSpout(Bean.class, "f()", "a", "b");
-		SpringBolt bolt1 = new SpringBolt(Bean.class, "f(a)", "y");
-		SpringBolt bolt2 = new SpringBolt(Bean.class, "f(b)", "z");
-		SpringBolt bolt3 = new SpringBolt(Bean.class, "f(y, z)");
-		test(spout, bolt1, bolt2, bolt3);
-		assertPassThrough(subject.get(spout).get(0), "b");
-		assertPassThrough(subject.get(spout).get(1), "y");
+		subject.add(spout("s1", "f()", "a", "b"));
+		subject.add(bolt("b1", "f(a)", "y"));
+		subject.add(bolt("b2", "f(b)", "z"));
+		subject.add(bolt("b3", "f(y, z)"));
+
+		assertPipeline("s1", "b1", "b2", "b3");
+		assertPassThrough("b1", "b");
+		assertPassThrough("b2", "y");
+		assertPassThrough("b3");
 	}
 
 	@Test
-	public void collect() {
-		SpringSpout spout = new SpringSpout(Bean.class, "f()", "feed");
-		SpringBolt bolt1 = new SpringBolt(Bean.class, "f(feed)", "a");
-		SpringBolt bolt2 = new SpringBolt(Bean.class, "f(a)", "b");
-		SpringBolt bolt3 = new SpringBolt(Bean.class, "f(feed, a, b)");
-		test(spout, bolt1, bolt2, bolt3);
-		assertPassThrough(subject.get(spout).get(0), "feed");
-		assertPassThrough(subject.get(spout).get(1), "feed", "a");
-	}
+	public void order() {
+		subject.add(spout("s1", "f()", "feed"));
+		subject.add(bolt("b1", "f(feed, a, b)"));
+		subject.add(bolt("b2", "f(a)", "b"));
+		subject.add(bolt("b3", "f(feed)", "a"));
 
-	@Test
-	public void reorder() {
-		SpringSpout spout = new SpringSpout(Bean.class, "f()", "feed");
-		SpringBolt bolt1 = new SpringBolt(Bean.class, "f(b)", "z");
-		SpringBolt bolt2 = new SpringBolt(Bean.class, "f(feed)", "b");
-		register(spout, bolt1, bolt2);
-		subject.run();
-		assertSequence(spout, bolt2, bolt1);
+		assertPipeline("s1", "b3", "b2", "b1");
+		assertPassThrough("b3", "feed");
+		assertPassThrough("b2", "feed", "a");
+		assertPassThrough("b1");
 	}
 
 	@Test
 	public void incomplete() {
-		SpringSpout spout = new SpringSpout(Bean.class, "f()", "feed");
-		SpringBolt bolt = new SpringBolt(Bean.class, "f(b)", "z");
-		register(spout, bolt);
+		subject.add(spout("s1", "f()", "feed"));
+		subject.add(bolt("b1", "f(b)", "z"));
 		subject.run();
-		assertSequence(spout);
+
+		try {
+			subject.verify();
+			fail("no verify exception");
+		} catch (IllegalStateException e) {
+			String expected = "Can't resolve all input fields for: [SpringBolt 'b1']";
+			assertEquals(expected, e.getMessage());
+		}
 	}
 
 	@Test
@@ -107,30 +122,67 @@ public class TopologyCompilationTest {
 		}
 	}
 
-	private void test(SpringSpout spout, SpringBolt... boltSequence) {
-		register(spout, boltSequence);
+	private void assertPipeline(String... expectedIdSequence) {
 		subject.run();
-		assertSequence(spout, boltSequence);
+		subject.verify();
+
+		String spoutId = expectedIdSequence[0];
+		SpringSpout spout = spoutById(spoutId);
+
+		List<SpringBolt> bolts = subject.get(spout);
+		int boltCount = bolts.size();
+
+		String[] actualIdSequence = new String[boltCount + 1];
+		actualIdSequence[0] = spoutId;
+		for (int i = 0; i < boltCount; ) {
+			String id = bolts.get(i).getId();
+			actualIdSequence[++i] = id;
+		}
+
+		assertArrayEquals("component ID", expectedIdSequence, actualIdSequence);
 	}
 
-	private void register(SpringSpout spout, SpringBolt... bolts) {
-		spout.setId(Integer.toHexString(spout.hashCode()));
-		for (SpringBolt b : bolts) b.setId(Integer.toHexString(b.hashCode()));
+	private void assertPassThrough(String boltId, String... fieldNames) {
+		SpringBolt bolt = boltById(boltId);
 
-		subject.add(spout);
-		subject.add(bolts);
+		Set<String> expected = new HashSet<>();
+		Set<String> actual = new HashSet<>();
+		Collections.addAll(expected, fieldNames);
+		Collections.addAll(actual, bolt.getPassThroughFields());
+		assertEquals("field names", expected, actual);
 	}
 
-	private void assertSequence(SpringSpout spout, SpringBolt... bolts) {
-		assertEquals("spout entry", true, subject.containsKey(spout));
-		assertEquals("bolt sequence", asList(bolts), subject.get(spout));
+	private SpringSpout spoutById(String id) {
+		Set<SpringSpout> availableSpouts = subject.keySet();
+		for (SpringSpout key : availableSpouts)
+			if (id.equals(key.getId()))
+				return key;
+
+		fail("missing spout " + id + " in " + availableSpouts);
+		return null;
 	}
 
-	private static void assertPassThrough(SpringBolt bolt, String... expected) {
-		String[] actual = bolt.getPassThroughFields();
-		sort(actual);
-		sort(expected);
-		assertArrayEquals(expected, actual);
+	private SpringBolt boltById(String id) {
+		Collection<List<SpringBolt>> availableBolts = subject.values();
+		for (List<SpringBolt> boltSequence : availableBolts)
+			for (SpringBolt entry : boltSequence)
+				if (id.equals(entry.getId()))
+					return entry;
+
+		fail("missing bolt " + id + " in " + availableBolts);
+		return null;
+	}
+
+	private static SpringSpout spout(String id, String signature, String... outputFields) {
+		SpringSpout spout = new SpringSpout(Bean.class, signature, outputFields);
+		spout.setId(id);
+		return spout;
+	}
+
+	private static SpringBolt bolt(String id, String signature, String... outputFields) {
+		SpringBolt bolt = new SpringBolt(Bean.class, signature, outputFields);
+		bolt.setId(id);
+		return bolt;
 	}
 
 }
